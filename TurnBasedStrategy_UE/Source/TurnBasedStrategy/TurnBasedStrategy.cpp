926a5723c39cc80faee78e324bbf72e0d03619e3
kym <kym0761@naver.com>
2023-03-27 01:57:22 +0900

    Remain Only Unity

--- TurnBasedStrategy_UE/Source/TurnBasedStrategy/TurnBasedStrategy.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TurnBasedStrategy.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TurnBasedStrategy, "TurnBaseStrategy" );
-
-DEFINE_LOG_CATEGORY(LogTurnBasedStrategy)
- 
